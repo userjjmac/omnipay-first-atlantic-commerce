9aace56e4c969269f7d9924fd20a7e793c43eb33
Kendall Arneaud <kendall.arneaud@gmail.com>
2021-06-12 15:46:32 -0400

    Update getCardReference method
    Fix reference to key in response data ara

--- src/Message/Response.php
+++ src/Message/Response.php
@@ -104,7 +104,15 @@ class Response extends AbstractResponse
      */
     public function getCardReference()
     {
-        return array_key_exists('TokenizedPAN', $this->data) ? $this->data['TokenizedPAN'] : ( array_key_exists('Token', $this->data) ? $this->data['Token'] : null);
+        return ( 
+                array_keys_exists('CreditCardTransactionResults', $this->data) 
+                && array_keys_exists('TokenizedPAN', $this->data['CreditCardTransactionResults'])
+               )? $this->data['CreditCardTransactionResults']['TokenizedPAN'] : 
+                ( array_key_exists('TokenizedPAN', $this->data) ? $this->data['TokenizedPAN'] : 
+                    ( array_key_exists('Token', $this->data) ? $this->data['Token'] : 
+                        null 
+                    )
+                );
     }
 
 
